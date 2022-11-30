d07219ca89b666cf31b33c2dd5c80d954e1c6152
HoiDam <66233626+HoiDam@users.noreply.github.com>
2022-11-30 17:13:43 +0800

    feat: step 1 finsihed

--- src/main/java/ooss/Person.java
+++ src/main/java/ooss/Person.java
@@ -1,4 +1,34 @@
 package ooss;
 
 public class Person {
+    private final String name;
+    private final int id;
+    private final int age;
+
+    public Person(int id,String name,int age){
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Person person = (Person) o;
+
+        return id == person.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return id;
+    }
+
+    public String introduce(){
+        return String.format("My name is %s. I am %d years old.",name,age);
+    }
+
+
 }

d11e2dd618b0d24c4251d23629addb4cd8099c96
WONGJE4 <wongje4@oocl.com>
2022-11-30 21:01:05 +0800

    feat: finish implementing Kclass

--- src/main/java/ooss/Klass.java
+++ src/main/java/ooss/Klass.java
@@ -1,4 +1,23 @@
 package ooss;
 
 public class Klass {
+    private final int number;
+
+    public Klass(int number) {
+        this.number = number;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if(!(obj instanceof Klass)) return false;
+
+        Klass klass = (Klass) obj;
+
+        return klass.number == number;
+    }
+
+    public int getNumber(){
+        return number;
+    }
 }
